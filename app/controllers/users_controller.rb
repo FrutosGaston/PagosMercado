5b1e8587b7d8b0bf8a2775d9fe3e7acf8fc6c06d
FrutosGaston <frutos.gaston@gmail.com>
2018-03-24 13:55:13 -0300

    Cambio los estados y code que devuelven los metodos de users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ class UsersController < ApplicationController
   def create
     User.create!(user_params)
 
-    head :ok
+    render json: { status: 'success', code: 200, message: 'Usuario creado correctamente' }
   end
 
   def balance
     user = User.find_by!(user_params)
 
-    render json: { status: 200, message: "Su saldo actual es de #{user.balance}" }
+    render json: { status: 'success', code: 200, message: "Su saldo actual es de #{user.balance}" }
   end
 
   private
