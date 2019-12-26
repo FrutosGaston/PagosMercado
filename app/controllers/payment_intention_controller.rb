fb34b3c572b167db097cfb4b50ac1deb7fefe04c
Gaston <frutos.gaston@gmail.com>
2019-12-26 01:10:40 -0300

    refactor - saco validaciones de tarjeta a otro objeto

--- app/controllers/payment_intention_controller.rb
+++ app/controllers/payment_intention_controller.rb
@@ -1,11 +1,10 @@
 class PaymentIntentionController < ApplicationController
   def create
-    card_validation = validate_payment
-    user = User.find_by(username: intention_params[:username], password: intention_params[:password])
-    payment = Payment.create!(amount: intention_params[:amount])
+    card_validation = validate_card
 
     if card_validation[:valid]
-      json_response({message: 'La operacion se inicio con exito', token: payment.token }, 200)
+      payment = Payment.create!(amount: intention_params[:amount])
+      json_response({message: 'La operacion se inicio con exito', token: payment.token })
     else
       json_response({message: 'La tarjeta es invalida', errors: card_validation[:errors] }, 400)
     end
@@ -21,14 +20,15 @@ class PaymentIntentionController < ApplicationController
     @current_payment ||= Payment.find_by!(token: params[:token])
   end
 
-  def validate_payment
-    Payment.validate_credit_card(
-        card_number: intention_params[:card_number], expiration_year: intention_params[:expiration_year].to_i,
-        expiration_month: intention_params[:expiration_month].to_i, name: intention_params[:name]
-    )
+  def validate_card
+    credit_card_params = intention_params[:credit_card]
+    CreditCard.new(
+        credit_card_params[:card_number], credit_card_params[:expiration_year].to_i,
+        credit_card_params[:expiration_month].to_i, credit_card_params[:name], credit_card_params[:cvv]
+    ).validate
   end
 
   def intention_params
-    params.permit(:username, :password, :amount, :card_number, :expiration_year, :expiration_month, :name)
+    params.permit(:amount, credit_card: [:card_number, :expiration_year, :expiration_month, :name, :cvv])
   end
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,10 +1,5 @@
 class Payment < ApplicationRecord
   DIFFERENT_AMOUNTS = Class.new(StandardError)
-  INVALID_DATE_ERROR = 'La fecha de expiracion ya paso'.freeze
-  INVALID_NUMBER_ERROR = 'El numero de tarjeta no existe'.freeze
-  INVALID_NAME_ERROR = 'El nombre del portador es invalido'.freeze
-  VALID_CARD_NUMBER = '4509953566233704'.freeze
-  VALID_NAME = 'APRO'.freeze
 
   # belongs_to :user
 
@@ -12,23 +7,6 @@ class Payment < ApplicationRecord
   validates_presence_of :token, :amount
 
   after_initialize :assign_token
-
-  class << self
-    def validate_credit_card(card_number: nil, expiration_year: nil, expiration_month: nil, name: nil)
-      errors = []
-      errors << INVALID_DATE_ERROR unless valid_dates(expiration_year, expiration_month)
-      errors << INVALID_NUMBER_ERROR unless card_number == VALID_CARD_NUMBER
-      errors << INVALID_NAME_ERROR unless name == VALID_NAME
-
-      { valid: errors.empty?, errors: errors }
-    end
-
-    def valid_dates(expiration_year, expiration_month)
-      current_year = Date.today.strftime('%y').to_i
-      valid_month = expiration_year == current_year ? expiration_month > Date.today.month : true
-      expiration_year >= current_year && valid_month
-    end
-  end
 
   def validate_amount!(another_amount)
     raise DIFFERENT_AMOUNTS unless amount == another_amount
